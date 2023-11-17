a2ab40fb6caca84aa7e099fcad8505495d34b4d4
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2023-11-17 22:13:00 +0700

    fix:text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 function Header() {
     return (
       <header className="mb-10 text-center">
-          <h1 className="text-4xl md:text-6xl font-bold text-white-800 font-noto-sans-thai">
+          <h1 className="text-4xl text-white-800 font-noto-sans-thai md:text-6xl font-bold text-white-800 font-noto-sans-thai">
             ค้นหาวิชา 🌙
           </h1>
           <p className="mt-2 text-lg text-white font-bold">
